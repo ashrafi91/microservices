bff3a76137c1bfd293b89242e058a1e30765736f
Adnan Ferdous Ashrafi <nazib91@yahoo.com>
2023-09-05 03:07:17 +0600

    clients,posts,comments

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import PostCreate from './PostCreate';
+
+export default()=>{
+    return <div className='container'><h1>Create post</h1>
+    <PostCreate/>
+    </div>;
+};
