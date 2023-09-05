f3935afa744c05524bd66dc8779b2d5de429da63
Adnan Ferdous Ashrafi <nazib91@yahoo.com>
2023-09-05 17:48:18 +0600

    Posts and comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default()=>{
     return <div className='container'><h1>Create post</h1>
     <PostCreate/>
+    <hr/>
+    <h1>Post List</h1>
+    <PostList/>
     </div>;
 };
