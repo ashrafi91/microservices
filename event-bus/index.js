20aa7a600025484c32c57f888ecd0fa0db09176a
Adnan Ferdous Ashrafi <nazib91@yahoo.com>
2023-09-08 02:01:59 +0600

    A mini microservices app

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios= require('axios');
-const cors = require('cors');
+
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
+
+
+const events = [];
 
 app.post('/events', (req, res)=>{
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
@@ -16,7 +21,10 @@ app.post('/events', (req, res)=>{
     axios.post('http://localhost:4003/events', event);
 
     res.send({status: 'Ok'});
+});
 
+app.get('/events', (req, res)=>{
+    res.send(events);
 });
 
 app.listen(4005, ()=>{
